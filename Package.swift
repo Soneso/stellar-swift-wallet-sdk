5fc3880184bb620fd7b59b83a327d36ebdff2c92
Christian Rogobete <c.rogobete@soneso.com>
2024-09-20 15:56:29 +0200

    improve project structure, add platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "stellar-wallet-sdk",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,7 +18,7 @@ let package = Package(
     dependencies: [
             // Dependencies declare other packages that this package depends on.
         //.package(url: "git@github.com:Soneso/stellar-ios-mac-sdk.git", from: "2.6.6"),
-        .package(path: "../../stellar-ios-mac-sdk")
+        .package(path: "../stellar-ios-mac-sdk")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
