994ae689404af4bde16513ab5099e53ac5d56e84
Christian Rogobete <c.rogobete@soneso.com>
2025-08-01 17:18:23 +0200

    update to use ios sdk v. 3.2.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Soneso/stellar-ios-mac-sdk", .upToNextMajor(from: "3.2.0")),
+        .package(url: "https://github.com/Soneso/stellar-ios-mac-sdk", .upToNextMajor(from: "3.2.1")),
         //.package(url: "https://github.com/Soneso/stellar-ios-mac-sdk", .revision("65b19d48990792cbcc7ff362a5c93ca8d7e5a1a1")),
         //.package(path: "../stellar-ios-mac-sdk") // if it is not updateing, you can use fix_spm_cache.sh
     ],
